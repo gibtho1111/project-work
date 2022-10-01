653ec69d17fcfe73957a88d6fd42d19496c37d3b
gibtho111 <89614783+gibtho111@users.noreply.github.com>
2022-10-01 19:56:47 +1000

    landing page finished basic structure

---
+++ app.js
@@ -0,0 +1,30 @@
+new Vue({
+  el: "#checked",
+  data(){
+    return {
+        checkedNames: [],
+    }
+  }
+
+});
+
+new Vue({
+  el: "#inputs",
+  data() {
+      return {
+          message: '',          
+      }
+  },
+methods: {
+
+},
+computed: {
+
+}
+});
+
+new Vue({
+  el:"#prop",
+  
+
+})
